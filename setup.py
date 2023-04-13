cfa0300fd6200d34628a7ec81f38d30a557e258a
Jaydeep Agravat <jaydeepagravat909@gmail.com>
2023-04-13 11:18:27 +0530

    include all file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="census-income",
+    version="0.0.1",
+    author="Jaydeep",
+    author_email="jaydeepagravat990@gmail.com",
+    packages=find_packages(),
+    install_requires = ["numpy"]
+)
